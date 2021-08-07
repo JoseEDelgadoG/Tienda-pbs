0246f498a687edad3b15ca2371046b01386e0edc
Jose Delgado <jose.enrique.vlc@gmail.com>
2021-08-06 23:51:15 -0300

    Proyecto ENK

---
+++ src/components/Product.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+import { ProductConsumer } from '../Context'
+
+
+
+export default class Product extends Component {
+    render() {
+        const {id, title, img, price, inCart } = this.props.product;
+        return (
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                 <div className="card">
+                 <div className="img-container p-5" onClick={console.log("Mensajes de click")}>
+                  <Link to="/details">
+                    <img src={img} alt="" className="card-img-top" />
+                  </Link>
+                 </div>
+                 </div>
+            </ProductWrapper>
+        );
+    }
+}
+
+const ProductWrapper = styled.div`
+
+`
